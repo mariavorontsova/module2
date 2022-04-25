c3bff8165ef1d4d44a84a0332b5fda8357311124
mariavorontsova <vorontsovamaria1994@gmail.com>
2022-04-25 18:09:31 +0400

    added to check_3

--- my.js
+++ my.js
@@ -12,9 +12,16 @@ buttonToDo.addEventListener('click', (e) => {
 buttonSort.addEventListener('click', (e) => {
     clickSortButton(e);
 }) 
-//document.addEventListener('keydown', (e) => e.key == enter)
+//добавила возможность добавления новой таски с нажатием Enter
+document.addEventListener("keydown", function (e) {
+    if (e.key !== "Enter") return;
+    clickButtonToDo(e);
+  });
 
 function clickButtonToDo (event) {
+    //добавила event.preventDefault(), т.к. есть добавление новой таски с нажанием Enter, что будет 
+    // обновлять станицу при каждом нажатии Enter
+    event.preventDefault();
     console.log('click'); //event.prevent ставим в том случае, 
     //если в индексе стоит button вместо div
 if (inputToDo.value === '' || inputToDo.value === ' ') { //проверка на пробел и пустую строку
@@ -22,6 +29,7 @@ if (inputToDo.value === '' || inputToDo.value === ' ') { //проверка на
     return
 } else {
     arrayToDo.push(inputToDo.value) //добавляем значение инпута в массив
+    console.log('arraToDO', arrayToDo);
     addTask(inputToDo.value) //вызов функции/передача строки, которую ввел пользователь
     inputToDo.value = ''; // зачищение инпута после ввода текста
     toDoList.style.display = 'block'; //todolist сделали видимым
@@ -42,14 +50,9 @@ newElementDiv.append(newDeleteButton);
 function createDeleteButton () {
 let deleteButton = document.createElement('button'); //создаем кнопку
 deleteButton.type = 'button'; //тип
-deleteButton.innerText = 'X'; 
+deleteButton.innerText = 'x'; 
 let deleteButtonClass = deleteButton.classList; 
-deleteButtonClass.add('delete-button');
-deleteButton.style.position = 'relative'; 
-deleteButton.style.border = '#C4C4C4';
-deleteButton.style.cursor = 'pointer';
-deleteButton.style.borderRadius = '50%';
-deleteButton.style.right = '16px';
+deleteButtonClass.add('delete-button'); //создали пустой div, стили перенесла в css
 deleteButton.addEventListener('click', clickDeleteButton);
 return deleteButton //результат выполнения функции
 }
@@ -68,43 +71,66 @@ console.log(arrayToDo);
 }
 
 function clickSortButton () {
-if (toDoList.innerText = '') {
-    return
-} else {
+    if (toDoList.style.display == 'block') {
+//if (toDoList.innerText = '') {
+   // return
+//} 
   let formSort = document.querySelectorAll('.text-list'); //находим все элементы с классом text-list
   arrayToDo.sort(compareList);//сортировка массива в который пушим task
   console.log(arrayToDo);
   //console.log(formSort);
 
-  formSort.forEach((element, i, array) => {
+  formSort.forEach((element, i, arr) => {
       arr[i].innerHTML = ''; //очистка каждого элемента
       arr[i].innerHTML = arrayToDo[i]; //вставляем значение из отсортированного массива
 
       let deleteButton = createDeleteButton();
-      element.append(deleteButton); 
+      element.append(deleteButton);
   })
   buttonSort.classList = buttonSort.classList = 'sort-button-list' 
   ? 'sort-button-list-two' : 'sort-button-list'
   //buttonSort.classList = buttonSort.classList == 'button-sort-list-one - стрелка сортировки вверх' ? 'button-sort-list-two 
   //- стрелка сортировки вниз' : 'button-sort-list-one - стрелка сортировки вверх'
+} else {
+    return
 }
-console.log(arrayToDo);
 }
 
+//в этой функции мы возвращаем паттерн сортировки в зависимости от того, какой класс 
+// присвоен кнопке сортировки- либо по возрастанию, либо по убыванию
+function compareList (a,b) { 
+    // return buttonSort.classList == 'sort-button-list'//проверяем на класс
+    if(buttonSort.classList == 'sort-button-list'){
+        if (a < b) {
+            return -1;
+          }
+          if (a > b) {
+            return 1;
+          }
+          return 0;
+       }else{
+        if (a < b) {
+            return 1;
+          }
+          if (a > b) {
+            return -1;
+          }
+          return 0; 
+       }
+    }
 
-function compareList (a,b) { // ? не поняла функцию
-return buttonSort.classList == 'sort-button-list'
-? a < b
-? -1
-: a > b 
-? 1
-: 0
-: a < b 
-? 1
-: a > b 
-? -1
-: 0;
-}
+
+    // ? a < b
+    // ? -1
+    // : a > b 
+    // ? 1
+    // : 0
+    // : a < b 
+    // ? 1
+    // : a > b 
+    // ? -1
+    // : 0;
+    // }
 
 
 
